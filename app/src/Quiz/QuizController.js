8e74e1bef99941c185d497f76b3c2ab6ff01c25f
Julien PETIT <contact@julienpetit.fr>
2015-10-07 23:06:01 +0200

    Optimizing theme training menu radio

--- app/src/Quiz/QuizController.js
+++ app/src/Quiz/QuizController.js
@@ -36,7 +36,7 @@
             });
 
 
-        $scope.questionsCountSelection = [ 5, 10, 15, 20, 30, 50 ];
+        $scope.questionsCountSelection = [ 5, 10, 15, 20, 30 ];
 
         // *********************************
         // Internal methods
